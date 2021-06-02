2b3dee48f45024f3adc0ec75d7ebbbbd2388ccb4
user <bipinaghimire6@gmail.com>
2021-06-02 18:51:30 +0545

    area

---
+++ square.py
@@ -0,0 +1,6 @@
+'''
+Assign a value to l to find the area of square. Here l represents the length of square.
+'''
+l= int(input("enter the length of square: "))
+area= l**2
+print(f'the area of square is {area}')
