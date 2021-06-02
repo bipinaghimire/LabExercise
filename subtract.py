9b3ebc95adaa66a8e48e419d10603e6d42530434
user <bipinaghimire6@gmail.com>
2021-06-02 19:29:38 +0545

    subtraction

---
+++ subtract.py
@@ -0,0 +1,8 @@
+'''
+Assign values to num1, num2 and num3. The find the subtraction of these numbers
+'''
+num1 = int(input('enter the value of num1:'))
+num2 = int(input('enter the value of num2:'))
+num3 = int(input('enter the value of num3:'))
+sub = num1-num2-num3
+print(f'the final value is ={sub}')
