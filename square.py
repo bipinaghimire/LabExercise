e43df9fe632de07d29b0bd94b7e6fdf3f16b8cc9
user <bipinaghimire6@gmail.com>
2021-06-02 19:05:38 +0545

    sum of three

--- square.py
+++ square.py
@@ -1,5 +1,5 @@
 '''
-Assign a value to l to find the area of square. Here l represents the length of square.
+find the area of square where value is assigned to l. Here l represents the length of square.
 '''
 l= int(input("enter the length of square : "))
 area= l**2
---
+++ sum.py
@@ -0,0 +1,8 @@
+'''
+assign values to num1, num2, and num3. Then findd the sum of these numbers
+'''
+num1 = int(input('enter the first value:'))
+num2 = int(input('enter the second value:'))
+num3 = int(input('enter the third value:'))
+sum = num1 + num2 + num3
+print(f'The sum of three number is {sum}')
